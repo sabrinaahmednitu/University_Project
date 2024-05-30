b5838e4282ec900305b39e3828245a659ac88cf6
sabrina <sabrinaahmednitu@gmail.com>
2024-05-30 23:30:11 +0600

    academic Semester controller and routes

---
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -0,0 +1,18 @@
+import httpStatus from "http-status";
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+
+const createAcademicSemester = catchAsync(async (req, res) => {
+    
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Academic semester created successfully !',
+        data: result,
+    });
+});
+
+export const AcademicSemesterControllers = {
+    createAcademicSemester,
+}
