d08b790dcd335e862885152d7101618a91f09504
sabrina <sabrinaahmednitu@gmail.com>
2024-05-31 00:54:43 +0600

    academic Semester repeation blocked

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -38,6 +38,20 @@ const academicSemesterSchema = new Schema<AcademicSemester>(
   }
 );
 
+// 2030 e jate fall semester 2 bar create na hoy
+//mongoose er (next)
+academicSemesterSchema.pre('save', async function (next) {
+  const isSemesterExists = await AcademicSemesterModel.findOne({
+    //this er modde user er dawa data ta k pabo
+    year: this.year,
+    name: this.name,
+  });
+  if (isSemesterExists) {
+    throw new Error('Semester is already esists !');
+  }
+  next();
+});
+
 // 3. Create a Model.
 export const AcademicSemesterModel = model<AcademicSemester>(
   'AcademicSemester',
