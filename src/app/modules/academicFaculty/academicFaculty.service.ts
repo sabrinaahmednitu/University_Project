150a91a5f8aa7c324279a0d33386aa9c89ced567
sabrina <sabrinaahmednitu@gmail.com>
2024-06-02 00:55:46 +0600

    academic faculty services

---
+++ src/app/modules/academicFaculty/academicFaculty.service.ts
@@ -0,0 +1,38 @@
+import { TAcademicFaculty } from './academicFaculty.interface';
+import { AcademicFacultyModel } from './academicFaculty.model';
+
+const createacademicFacultyIntoDB = async (payload: TAcademicFaculty) => {
+  const result = await AcademicFacultyModel.create(payload);
+  return result;
+};
+
+const getAllAcademicFacultiesFromDB = async () => {
+  const result = await AcademicFacultyModel.find();
+  return result;
+};
+
+const getSingleAcademicFacultiesFromDB = async (id: string) => {
+  const result = await AcademicFacultyModel.findById(id);
+  return result;
+};
+
+const updateAcademicFacultyFromDB = async (
+  id: string,
+  payload: Partial<TAcademicFaculty>
+) => {
+  const result = await AcademicFacultyModel.findByIdAndUpdate(
+    { _id: id },
+    payload,
+    {
+      new: true,
+    }
+  );
+  return result;
+};
+
+export const academicFacultyServices = {
+  createacademicFacultyIntoDB,
+  getAllAcademicFacultiesFromDB,
+  getSingleAcademicFacultiesFromDB,
+  updateAcademicFacultyFromDB,
+};
