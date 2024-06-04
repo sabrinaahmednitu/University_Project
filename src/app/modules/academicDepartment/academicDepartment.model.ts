5910cd069fdcdfe13dd445baa41555adb5342567
sabrina <sabrinaahmednitu@gmail.com>
2024-06-05 03:14:04 +0600

    academic departments errors handled

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -18,6 +18,28 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   }
 );
 
+//department name duplicate hobena
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExists = await AcademicDepartmentModel.findOne({
+    name: this.name,
+  });
+  if (isDepartmentExists) {
+    throw new Error('This department is already esists!');
+  }
+  next();
+})
+
+//query middleware diye delete id update howa atkaschi 
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+ const isDepartmentExists = await AcademicDepartmentModel.findOne(query);
+   if (!isDepartmentExists) {
+     throw new Error('This department does not esists!');
+   }
+   next();
+});
+
+//model
 export const AcademicDepartmentModel = model<TAcademicDepartment>(
   'AcademicDepartment',
   academicDepartmentSchema,
