191e2cdb3cddd2897501d2c71fb4d35a7fdf544c
sabrina <sabrinaahmednitu@gmail.com>
2024-06-04 23:46:10 +0600

    academicDepartment services

---
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -0,0 +1,36 @@
+import { TAcademicDepartment } from './academicDepartment.interface';
+import { AcademicDepartmentModel } from './academicDepartment.model';
+
+const createAcademicDepartmentIntoDB = async (payload: TAcademicDepartment) => {
+  const result = await AcademicDepartmentModel.create(payload);
+  return result;
+};
+
+const getAllAcademicDepartmentFromDB = async () => {
+  const result = await AcademicDepartmentModel.find();
+  return result;
+};
+const getSingleAcademicDepartmentFromDB = async (id: string) => {
+  const result = await AcademicDepartmentModel.findById(id);
+  return result;
+};
+const updateAcademicDepartmentIntoDB = async (
+  id: string,
+  payload: Partial<TAcademicDepartment>
+) => {
+  const result = await AcademicDepartmentModel.findOneAndUpdate(
+    { _id: id },
+    payload,
+    {
+      new: true,
+    }
+  );
+  return result;
+};
+
+export const AcademicDepartmentServices = {
+  createAcademicDepartmentIntoDB,
+  getAllAcademicDepartmentFromDB,
+  getSingleAcademicDepartmentFromDB,
+  updateAcademicDepartmentIntoDB,
+};
