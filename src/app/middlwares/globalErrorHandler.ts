dec5b5c9fba7aa8e46a8e32a17b98d86a4c9bf02
sabrina <sabrinaahmednitu@gmail.com>
2024-06-10 01:53:45 +0600

    error and AppError solved

--- src/app/middlwares/globalErrorHandler.ts
+++ src/app/middlwares/globalErrorHandler.ts
@@ -1,6 +1,7 @@
 import { ErrorRequestHandler } from 'express';
 import { ZodError } from 'zod';
 import config from '../config';
+import AppError from '../errors/AppError';
 import handleCastError from '../errors/handleCastError';
 import handleDuplicateError from '../errors/handleDuplicateError';
 import handleValidationError from '../errors/handleValidationError';
@@ -9,8 +10,8 @@ import { TErrorSources } from '../interface/error';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   //setting default value
-  let statusCode = err.statusCode || 500;
-  let message = err.message || 'something went wrong!';
+  let statusCode =  500;
+  let message = 'something went wrong!';
 
   //default konokichu na mille ai pathabe
   let errorSources: TErrorSources = [
@@ -41,13 +42,38 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
   }
-  //customized mongoose code:11000 errror
+  //customized mongoose code:11000 errror/Duplicate name solve error
   else if (err?.code === 11000) {
     const simplifiedError = handleDuplicateError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
   }
+  //customized AppError solve
+  else if (err instanceof AppError) {
+    statusCode = err?.statusCode;
+    message = err?.message;
+    errorSources = [
+      {
+        path: '',
+        message:err?.message
+      }
+    ];
+  }
+  //customized Error solve
+  else if (err instanceof Error) {
+    message = err?.message;
+    errorSources = [
+      {
+        path: '',
+        message:err?.message
+      }
+    ];
+  }
+
+
+
+
   //ultimate return
   return res.status(statusCode).json({
     success: false,
