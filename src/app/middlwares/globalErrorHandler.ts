ffd04ddaf440a4c04e912723d79bccd1a96ed90a
sabrina <sabrinaahmednitu@gmail.com>
2024-06-09 23:19:29 +0600

    mongoose error handled

--- src/app/middlwares/globalErrorHandler.ts
+++ src/app/middlwares/globalErrorHandler.ts
@@ -1,8 +1,9 @@
 import { ErrorRequestHandler } from 'express';
 import { ZodError } from 'zod';
 import config from '../config';
+import handleValidationError from '../errors/handleValidationError';
 import handleZodError from '../errors/handleZodError';
-import { TErrorSource } from '../interface/error';
+import { TErrorSources } from '../interface/error';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   //setting default value
@@ -10,27 +11,35 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let message = err.message || 'something went wrong!';
 
   //default konokichu na mille ai pathabe
-  let errorSources: TErrorSource = [
+  let errorSources: TErrorSources = [
     {
       path: '',
       message: 'something went Wrong',
     },
   ];
 
+
+  //customized zod validation errror
   if (err instanceof ZodError) {
     const simplifiedError = handleZodError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
   }
-
-  return res.status(statusCode).json({
-    success: false,
-    message,
-    errorSources,
-    stack:config.NODE_ENV==='development'?err?.stack :null,
-    //AmiError: err,
-  });
+  //customized mongoose validation errror
+  else if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  }
+    return res.status(statusCode).json({
+      success: false,
+      message,
+      errorSources,
+      stack: config.NODE_ENV === 'development' ? err?.stack : null,
+      //AmiError: err,
+    });
 };
 
 export default globalErrorHandler;
